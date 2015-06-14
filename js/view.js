482a8a24802a0712d66708252499103f6ca86cbf
Beck <1032311756@qq.com>
2015-06-14 18:32:03 +0800

    file

---
+++ js/view.js
@@ -0,0 +1,172 @@
+// JavaScript Document
+
+$(function(){
+
+           $("#div1").hide();  
+
+            $("#in1").click(function(){
+
+                  $("#div1").fadeIn("slow");
+
+            });
+
+            $("#out1").click(function(){
+
+                     $("#div1").fadeOut("slow"); 
+
+            })
+			
+
+        });
+		
+$(function(){
+
+           $("#div2").hide();  
+
+            $("#in2").click(function(){
+
+                  $("#div2").fadeIn("slow");
+
+            });
+
+            $("#out2").click(function(){
+
+                     $("#div2").fadeOut("slow"); 
+
+            })
+			
+
+        });
+		
+$(function(){
+
+           $("#div3").hide();  
+
+            $("#in3").click(function(){
+
+                  $("#div3").fadeIn("slow");
+
+            });
+
+            $("#out3").click(function(){
+
+                     $("#div3").fadeOut("slow"); 
+
+            })
+			
+
+        });
+		
+$(function(){
+
+           $("#div4").hide();  
+
+            $("#in4").click(function(){
+
+                  $("#div4").fadeIn("slow");
+
+            });
+
+            $("#out4").click(function(){
+
+                     $("#div4").fadeOut("slow"); 
+
+            })
+			
+
+        });
+		
+$(function(){
+
+           $("#div5").hide();  
+
+            $("#in5").click(function(){
+
+                  $("#div5").fadeIn("slow");
+
+            });
+
+            $("#out5").click(function(){
+
+                     $("#div5").fadeOut("slow"); 
+
+            })
+			
+
+        });
+		
+$(function(){
+
+           $("#div6").hide();  
+
+            $("#in6").click(function(){
+
+                  $("#div6").fadeIn("slow");
+
+            });
+
+            $("#out6").click(function(){
+
+                     $("#div6").fadeOut("slow"); 
+
+            })
+			
+
+        });
+		
+$(function(){
+
+           $("#div7").hide();  
+
+            $("#in7").click(function(){
+
+                  $("#div7").fadeIn("slow");
+
+            });
+
+            $("#out7").click(function(){
+
+                     $("#div7").fadeOut("slow"); 
+
+            })
+			
+
+        });
+		
+$(function(){
+
+           $("#div8").hide();  
+
+            $("#in8").click(function(){
+
+                  $("#div8").fadeIn("slow");
+
+            });
+
+            $("#out8").click(function(){
+
+                     $("#div8").fadeOut("slow"); 
+
+            })
+			
+
+        });
+		
+$(function(){
+
+           $("#div9").hide();  
+
+            $("#in9").click(function(){
+
+                  $("#div9").fadeIn("slow");
+
+            });
+
+            $("#out9").click(function(){
+
+                     $("#div9").fadeOut("slow"); 
+
+            })
+			
+
+        });
